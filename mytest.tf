a7c698741dc60e6dcb5c67cecdd0c9563ecc0482
Varuni17 <64917475+Varuni17@users.noreply.github.com>
2020-06-14 22:24:42 +0530

    terraform file

--- mytest.tf
+++ mytest.tf
@@ -61,7 +61,7 @@ resource "aws_instance" "hybrid1os" {
 }
 resource "aws_volume_attachment" "ebs_att" {
 	device_name  = "/dev/sdc"
-	volume_id    = "${aws_ebs_volume.hybrid1.id}"
+	volume_id    = "${aws_ebs_volume.ebs1.id}"
 	instance_id  = "${aws_instance.hybrid1os.id}"
 	force_detach = true
 }
